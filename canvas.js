a37e6cf0a1b5db07ebed6a6ef28ce0d6881397e8
Henry Zhang <henryzhang9802@gmail.com>
2019-06-10 13:56:51 +0100

    Rect added

--- canvas.js
+++ canvas.js
@@ -1,5 +1,17 @@
 console.log("hello world.")
 var canvas = document.querySelector("canvas")
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; 
 var ctx = canvas.getContext("2d");
-ctx.fill(100,100,width,height)
+
 console.log(canvas)
+
+
+//create gradient
+var grd1 = ctx.createLinearGradient(0, 0, 200, 100);
+grd1.addColorStop(0, "#7837CF");
+grd1.addColorStop(1, "#C55E7B");
+
+ctx.fillStyle = grd1;
+ctx.fillRect(10, 10, 150, 80);
+
