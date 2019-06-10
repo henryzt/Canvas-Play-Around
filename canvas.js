d746642bc1f73ec859906363e648a3c5448ffc77
Henry Zhang <henryzhang9802@gmail.com>
2019-06-10 15:41:36 +0100

    New structure

--- canvas.js
+++ canvas.js
@@ -2,42 +2,34 @@ console.log("hello world.")
 
 var box;
 
-
-
-
-function createObjects(){
-    //create gradient
-    box = new component(10,10,200,200)
-    box.speedX = 1;
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
 }
 
+
 function createGraditent(x,y,w,h){
-    var grd1 = myCanvas.ctx.createLinearGradient(x,y,w,h);
+    var grd1 = ctx.createLinearGradient(x,y,w,h);
     grd1.addColorStop(0, "#7837CF");
     grd1.addColorStop(1, "#C55E7B");
     return grd1
 }
 
-var myCanvas = {
-    canvas : document.querySelector("canvas"),
-    start : function() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight; 
-        this.ctx = this.canvas.getContext("2d");
-        this.interval = setInterval(updateArea, 20);
-        console.log(this.canvas)
-    },
-    clear : function() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    stop : function() {
-      clearInterval(this.interval);
-    }
-  }
+
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; 
+var ctx = this.canvas.getContext("2d");
+var box = new component(10,10,100,100)
+box.speedX = 10;
 
 
 
-  function component(x, y, width, height) {
+
+function component(x, y, width, height) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -45,21 +37,25 @@ var myCanvas = {
     this.x = x;
     this.y = y;    
     this.update = function() {
-        ctx = myCanvas.ctx;
         ctx.fillStyle = createGraditent(this.x, this.y, this.width, this.height);
         ctx.fillRect(this.x, this.y, this.width, this.height);
+        if(this.x + this.width > window.innerWidth || this.x < 0){
+            this.speedX = - this.speedX
+        }
+        if(this.y + this.height > window.innerHeight || this.y < 0){
+            this.speedY = - this.speedY
+        }
     }
-  }
+}
 
 
-function updateArea() {
-    myCanvas.clear();
+function animate() {
+    requestAnimationFrame(animate)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     box.x += box.speedX;
     box.y += box.speedY;    
     box.update();
 }
 
+animate()
 
-myCanvas.start()
-
-createObjects()
