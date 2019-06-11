4a9e38057b2da1bd0a26be436fd91a6c70e718e1
Henry Zhang <henryzhang9802@gmail.com>
2019-06-11 14:52:50 +0100

    Add colours and randomize start location

--- inward-stars/canvas.js
+++ inward-stars/canvas.js
@@ -1,5 +1,5 @@
 /*  created by Henry Zhang
-    coded at 10/Jun/2019    */
+    coded at 11/Jun/2019    */
 
 var canvas = document.querySelector("canvas");
 
@@ -14,27 +14,34 @@ window.addEventListener("resize", function(){
 })
 
 for(var i = 0; i < 400; i++){
-    var st = new star(window.innerWidth/2, window.innerHeight/2, 1)
+    var st = new star(window.innerWidth/2, window.innerHeight/2, 4)
     st.reset()
     stArray.push(st);
 }
 
 
+function randomColorPicker(){
+    var colors = ["195, 112, 255","142, 102, 232","129, 125, 255","102, 132, 232","112, 183, 255"]
+    return colors[Math.floor(Math.random()*colors.length)]
+}
+
+
 function star(x, y, radius) {
+    this.color = randomColorPicker();
     this.radius = radius;
     this.speedX = 0;
     this.speedY = 0;    
     this.x = x;
     this.y = y; 
     this.blur = 0;  
-    this.r=0
-    this.g=0
-    this.b=0
+    this.alpha=0
     this.first=true
     this.update = function() {
         ctx.beginPath()
         ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
-        ctx.fillStyle = `rgba(${this.r++},${this.r++},${this.r++})`
+        ctx.fillStyle = `rgba(${this.color}, ${this.alpha+=0.01})`
+        ctx.shadowBlur = 5;
+        ctx.shadowColor = "white";
         ctx.fill();
         this.x += this.speedX;
         this.y += this.speedY; 
@@ -49,15 +56,10 @@ function star(x, y, radius) {
         var directionY = Math.random() >= 0.5? 1 : -1;
         this.speedX = directionX * Math.random() ;
         this.speedY = directionY * Math.random() ;
-        var random = Math.random()
-        if(this.first){
-            this.x = window.innerWidth/2 + this.speedX * random * window.innerWidth/2
-            this.y = window.innerHeight/2 + this.speedY * random * window.innerHeight/2
-            this.first=false
-        }else{
-            this.x = window.innerWidth/2
-            this.y = window.innerHeight/2
-        }
+        var random = Math.random() //random displacement
+        this.x = window.innerWidth/2 + this.speedX * random * window.innerWidth/2
+        this.y = window.innerHeight/2 + this.speedY * random * window.innerHeight/2
+        this.alpha = 0
         
     }
 }
@@ -66,7 +68,7 @@ function star(x, y, radius) {
 function animate() {
     requestAnimationFrame(animate)
     // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+    ctx.fillStyle = "rgba(0, 0, 0, 1)"
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
     for(var i = 0; i < stArray.length; i++){
         stArray[i].update();
