6ffc99d1426ee2e53aa3218c5b0465dfb7c6315e
Henry Zhang <henryzhang9802@gmail.com>
2019-06-11 15:34:55 +0100

    Another fun accidental effect

--- inward-stars/canvas.js
+++ inward-stars/canvas.js
@@ -9,6 +9,7 @@ canvas.height = window.innerHeight;
 var stArray = []
 
 var onHold = false;
+var opacity = 1; //of the rect
 
 function startOnHold(){
     // document.getElementById("text").className = "center hidden";
@@ -47,7 +48,9 @@ function star(x, y, radius) {
     this.color = randomColorPicker();
     this.radius = radius;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;   
+    this.lastSpeedX = this.speedX
+    this.lastSpeedY = this.speedY
     this.x = x;
     this.y = y; 
     this.blur = 0;  
@@ -59,14 +62,25 @@ function star(x, y, radius) {
         ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
         if(this.alpha < this.maxAplha){ this.alpha+=0.01 }
         ctx.fillStyle = `rgba(${this.color}, ${this.alpha})`
-        if(onHold){
-            ctx.shadowBlur = 0
-        }else{
-            ctx.shadowBlur = 5;
-            ctx.shadowColor = "white";
-        }
+        // if(onHold){
+        //     ctx.shadowBlur = 0
+        // }else if(opacity == 1){
+        //     ctx.shadowBlur = 5;
+        //     ctx.shadowColor = `rgba(${this.color}, ${this.alpha})`
+        // }
+        
         
         ctx.fill();
+        if(onHold){
+            if(this.speedX < 10){
+                this.speedX +=0.1
+                this.speedY +=0.1
+            }
+        }
+        if(!onHold && this.speedX > this.lastSpeedX){
+            this.speedX -=0.1
+            this.speedY -=0.1
+        }
         this.x += this.speedX;
         this.y += this.speedY; 
         if((this.x + this.radius > window.innerWidth || this.x < 0) && (this.y + this.radius > window.innerHeight || this.y < 0)){
@@ -92,12 +106,14 @@ function star(x, y, radius) {
 function animate() {
     requestAnimationFrame(animate)
     // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    if(onHold){
-        ctx.fillStyle = "rgba(0, 0, 0, 0.01)"
-    }else{
-        ctx.fillStyle = "rgba(0, 0, 0, 1)"
+
+    if(onHold && opacity > 0.05){
+        opacity-=0.01
     }
-    
+    if(!onHold && opacity < 1){
+        opacity+=0.01
+    }
+    ctx.fillStyle = `rgba(0, 0, 0, ${opacity})`
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
     for(var i = 0; i < stArray.length; i++){
         stArray[i].update();
