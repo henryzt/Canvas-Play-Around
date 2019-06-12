65a9b7ff8696f9009ece76d4a6f66996689390e3
Henry Zhang <henryzhang9802@gmail.com>
2019-06-12 12:44:17 +0100

    Clear possible confusion

--- tilting-3d-card/script.js
+++ tilting-3d-card/script.js
@@ -1,6 +1,7 @@
 /*  created by Henry Zhang
     coded at 12/Jun/2019    */
 
+var imgUrl = "https://muchneededtherapydotblog.files.wordpress.com/2018/08/lockscreen_vultre-damask2.jpg"
 var card = document.getElementById("card");
 updateReflection(100,0)
 
@@ -10,15 +11,15 @@ window.addEventListener("mousemove", (event)=>{
     let mouseY = event.clientY
     let halfWidth = window.innerWidth/2
     let halfHeight = window.innerHeight/2 
-    let xdeg = (mouseX - halfWidth)/halfWidth * 10;
-    let ydeg = (mouseY - halfHeight)/halfHeight * 10;
-    updateReflection(ydeg * 18, xdeg * 10)
-    card.style.transform = `rotateX(${ydeg}deg) rotateY(${xdeg}deg)`;
+    let xdeg = (mouseX - halfWidth)/halfWidth;
+    let ydeg = (mouseY - halfHeight)/halfHeight;
+    updateReflection(ydeg * 180, xdeg * 100)
+    card.style.transform = `rotateX(${ydeg * 10}deg) rotateY(${xdeg * 10}deg)`;
 })
 
 
 function updateReflection(degree,percentage){
-    card.style.background = `linear-gradient(${degree}deg, rgba(255,255,255,0) 0%,rgba(255,255,255,0.5) ${percentage}%,rgba(255,255,255,0) 100%), url('https://muchneededtherapydotblog.files.wordpress.com/2018/08/lockscreen_vultre-damask2.jpg')`
+    card.style.background = `linear-gradient(${degree}deg, rgba(255,255,255,0) 0%,rgba(255,255,255,0.35) ${percentage}%,rgba(255,255,255,0) 100%), url('${imgUrl}')`
     card.style.backgroundSize = "cover"
 }
 
