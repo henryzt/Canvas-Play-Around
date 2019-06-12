6fdd8ab94b40c05fcb3b100b6805573a7cd5265e
Henry Zhang <henryzhang9802@gmail.com>
2019-06-12 11:48:51 +0100

    Basic 3d rotation

--- tilting-3d-card/script.js
+++ tilting-3d-card/script.js
@@ -1,13 +1,8 @@
 /*  created by Henry Zhang
-    coded at 10/Jun/2019    */
+    coded at 12/Jun/2019    */
 
-var canvas = document.querySelector("canvas");
 
-var ctx = this.canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight; 
-
-var mouse = {x:canvas.width/2, y:canvas.height/2}
+var mouse = {x:window.innerWidth/2, y:window.innerHeight/2}
 var clicked = false
 
 window.addEventListener("mousemove", (event)=>{
